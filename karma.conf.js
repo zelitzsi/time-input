cd148397920de0c3beecbff221459be31254066d
alanclarke <hi@alz.so>
2016-04-17 22:57:15 +0100

    improve karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,27 +1,16 @@
 var path = require('path')
 var webpackConfig = require('./webpack.config')
+webpackConfig.module.loaders.push({
+  test: /\.js$/,
+  include: [path.resolve('components/'), path.resolve('lib/')],
+  loader: 'isparta'
+})
 module.exports = function (config) {
   config.set({
     frameworks: ['mocha'],
-    files: ['test/**/test-*'],
-    preprocessors: {
-      'test/**/test-*': ['webpack', 'sourcemap']
-    },
-    webpack: Object.assign(webpackConfig, {
-      devtool: 'inline-source-map',
-      watch: true,
-      module: {
-        preLoaders: [{
-          test: /\.js$/,
-          exclude: [path.resolve('node_modules/')],
-          loader: 'babel'
-        }, {
-          test: /\.js$/,
-          include: [path.resolve('components/'), path.resolve('lib/')],
-          loader: 'isparta'
-        }]
-      }
-    }),
+    files: [ 'test/test-*' ],
+    preprocessors: { '/**/*.js': ['webpack', 'sourcemap'] },
+    webpack: webpackConfig,
     webpackServer: {
       quiet: true,
       noInfo: true
@@ -30,7 +19,7 @@ module.exports = function (config) {
     reporters: ['progress', 'coverage'],
     coverageReporter: {
       reporters: [
-        { type: 'html', dir: 'coverage' },
+        // { type: 'html', dir: 'coverage' },
         { type: 'text' }
       ]
     }
