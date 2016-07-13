55796157830a721cfbdd46b64e529894b557b4c4
alanclarke <hi@alz.so>
2016-07-13 23:33:02 +0100

    set default value in demo

--- demo/demo.js
+++ demo/demo.js
@@ -4,7 +4,7 @@ var TimeInput = require('../src/components/TimeInput')
 
 ;(function render (value) {
   ReactDom.render((
-    <TimeInput value={value} onChange={render}/>
+    <TimeInput value={value} onChange={render} defaultValue='12:00:00:000 AM'/>
   ), document.getElementById('demo-1'))
 })('12:00:00:000 AM')
 
