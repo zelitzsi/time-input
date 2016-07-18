b4f3e1006c4c25a1502acd1d42c409adefd2a00b
alanclarke <hi@alz.so>
2016-07-18 23:38:09 +0100

    test classnames

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # time-input
 A keyboard friendly react component for capturing time
 
-## goals
-- small ui surface area (just a form input)
+## features
+- small UI surface area (just a form input)
 - keyboard friendly (can type times, use up and down keys to go forwards and backwards in time, can tab between time groups)
 - simple api (infers most options from value, e.g. 24hr time or 12hr, whether to display seconds and milliseconds)
+- easy going UX: ignores invalid input and simply skips over separator if omitted
+- no dependencies
+- 95% test coverage
 
 ## installation
 ```
---
+++ test/test-classnames.js
@@ -0,0 +1,16 @@
+/* global describe it */
+var React = require('react')
+var ReactDOM = require('react-dom')
+var expect = require('chai').expect
+var TimeInput = require('../src/components/TimeInput')
+
+describe('classnames', function () {
+  it('should render any provided classnames additionally', function () {
+    document.body.innerHTML = '<div></div>'
+    ReactDOM.render((
+      <TimeInput className='extra-1 extra-2' />
+    ), document.body.firstElementChild)
+    var el = document.body.getElementsByTagName('input')[0]
+    expect(el.parentElement.className).to.eql('TimeInput extra-1 extra-2')
+  })
+})
